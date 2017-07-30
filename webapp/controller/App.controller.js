261b9dd9de1556af18adae14059909ce20b19169
Balint Szebenyi <szebenyib@gmail.com>
2017-07-30 17:42:07 +0200

    Adding first pages to load

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+	"breeze/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
+	"use strict";
+	return BaseController.extend("breeze.controller.App", {
+		onInit: function() {
+			var oViewModel,
+					fnSetAppNotBusy,
+					iOriginalBusyDelay = this.getView()
+						.getBusyIndicatorDelay();
+			oViewModel = new JSONModel({
+				busy: true,
+				delay: 0
+			});
+			this.setModel(oViewModel, "appView");
+			fnSetAppNotBusy = function() {
+				oViewModel.setProperty("/busy", false);
+				oViewModel.setProperty("/delay",
+				iOriginalBusyDelay);
+			};
+			this.getOwnerComponent()
+				.getModel()
+				.metadataLoaded()
+				.then(fnSetAppNotBusy);
+		// apply content density mode to root view
+			/*definethis.getView()
+							.addStyleClass(this.getOwnerComponent()
+												.getContentDensityClass());*/
+		}
+	});
+});
---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"breeze/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+], function(BaseController, JSONModel) {
+	"use strict";
+
+	return BaseController.extend("breeze.controller.Main", {
+			onInit: function() {
+			}
+	});
+});
