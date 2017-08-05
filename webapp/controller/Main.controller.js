118e046779dab205a9c53d363fc75701e72b8c66
Balint Szebenyi <szebenyib@gmail.com>
2017-08-05 16:13:28 +0200

    Adding navigation between split app master detail

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,9 +9,29 @@ sap.ui.define([
 		oACDevicesModel: new JSONModel(),
 		onInit: function() {
 			Models.loadACDevicesModel(this);
+			this.byId("detailPage").setShowNavButton(this.isDevicePhone());
 		},
 		onACDevicesModelLoaded: function() {
-			this.getView().setModel(this.oACDevicesModel, "ACDevicesModel");
+			this.setModel(this.oACDevicesModel, "ACDevicesModel");
+		},
+		onDevicePressed: function(oEvent) {
+			var sDevId = oEvent.getSource().getValue();
+			this.setDevModelByDevId(sDevId);
+			this.byId("splitContainer")
+				.toDetail(this.createId("detailPage"));
+		},
+		setDevModelByDevId: function(sDevId) {
+			var oDevices = this.getModel("ACDevicesModel").getProperty("/");
+			for (var i = 0; i < oDevices.length; i++) {
+				if (oDevices[i].id === sDevId) {
+					var oSelDevModel = new JSONModel(oDevices[i]);
+					this.setModel(oSelDevModel, "SelDevModel");
+				}
+			}
+		},
+		onNavButtonPressed: function() {
+			this.byId("splitContainer")
+				.toMaster(this.createId("masterPage"));
 		}
 	});
 });
